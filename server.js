7c79fa9aaba3a309646dada4d33c48fc7608f1ad
dustindo1 <duhstin0@gmail.com>
2025-11-03 20:24:45 -0800

    update

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
 // Your server code goes here. Host your server on localhost:3000
+const http = require('http');
+const fs = require('fs');
+
+
+const server = http.createServer((req, res) => {
+    if(req.url === '/' || req.url === '/index.html') {
+        fs.readFile('index.html', (err, data) => {
+            if (err) {
+                res.statusCode = 500;
+                res.end('Error loading index.html');
+            } else {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/html');
+                res.end(data);
+            }
+        });
+       } else {
+        res.statusCode = 404;
+        res.end('404 Not Found');
+    }
+});
+
+server.listen(3000, function() {
+    console.log('Server is listening on port 3000');
+});
